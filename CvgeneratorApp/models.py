28a2fd46c250211fed9456887d74cf6a70b27841
KhushbooItIs <workkhushboo16@gmail.com>
2026-03-24 21:28:16 +0530

    form posted

--- CvgeneratorApp/models.py
+++ CvgeneratorApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=100)
+    phone=models.CharField(max_length=20)
+    school=models.CharField(max_length=50)
+    degree=models.CharField(max_length=100)
+    university=models.CharField(max_length=100)
+    summary=models.TextField(max_length=2000)
+    previous_work=models.TextField(max_length=1000)
+    skil=models.CharField(max_length=200)
--- CvgeneratorApp/views.py
+++ CvgeneratorApp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+def save_profile(request):
+
+    if request.method == 'POST':   
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        school = request.POST.get('school')
+        degree = request.POST.get('degree')
+        university = request.POST.get('university')
+        summary = request.POST.get('summary')
+        previous_work = request.POST.get('previous_work')
+        skil = request.POST.get('skil')  
+
+        p = Profile(
+            name=name,
+            email=email,
+            phone=phone,
+            school=school,
+            degree=degree,
+            university=university,
+            summary=summary,
+            previous_work=previous_work,   
+            skil= skil               
+        )
+
+        p.save()  
+
+    return render(request, 'CvgeneratorApp/accept.html')
